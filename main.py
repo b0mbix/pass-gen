1d16bfd087414204ab8ce6eb622894cf4cd6b60f
Jakub Bąba <bombix.git@gmail.com>
2020-08-07 22:45:20 +0200

    Checking is input correct

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from generator import generating_pass
 
 print("Welcome to PassGen!")
-number = int(input("Please write your password lenght: "))
+try:
+    number = int(input("Please write your password lenght: "))
 
-print("What'll be included in password (y/n)?")
-details = []
-details.append(input("Lowercase characters: ").lower())
-details.append(input("Uppercase characters: ").lower())
-details.append(input("Numbers: ").lower())
-details.append(input("Symbols: ").lower())
+    print("What'll be included in password (y/n)?")
+    details = []
+    details.append(input("Lowercase characters: ").lower())
+    details.append(input("Uppercase characters: ").lower())
+    details.append(input("Numbers: ").lower())
+    details.append(input("Symbols: ").lower())
 
-print(generating_pass(number, details))
+    for i in details:
+        if i != 'y' and i != 'n':
+            raise ValueError
+
+    print(generating_pass(number, details))
+except ValueError:
+    print("Invalid input!")
