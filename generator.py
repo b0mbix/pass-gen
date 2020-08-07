3eedaf68e698fa3531241c8f6671049a025ce4a0
Jakub Bąba <bombix.git@gmail.com>
2020-08-07 19:43:13 +0200

    Added options which characters types'll be in the password

--- generator.py
+++ generator.py
@@ -1,11 +1,18 @@
 import random
-def generating_pass(number):
-    symbols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-    '!', '@', '#', '$', '%', '^', '&', '*', '?', '+', '=', '-']
+def generating_pass(number, options):
+    symbols = []
+    possible = [['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    ['!', '@', '#', '$', '%', '^', '&', '*', '?', '+', '=', '-']]
+
+    for i in range(4):
+        if options[i] == 'y':
+            for j in possible[i]:
+                symbols.append(j)
+
     chars = []
     for i in range(number):
-        chars.append(symbols[random.randint(0, 73)])
+        chars.append(symbols[random.randint(0, len(symbols)-1)])
     result = ''.join(chars)
     return result
--- main.py
+++ main.py
@@ -3,4 +3,11 @@ from generator import generating_pass
 print("Welcome to PassGen!")
 number = int(input("Please write your password lenght: "))
 
-print(generating_pass(number))
+print("What'll be included in password (y/n)?")
+details = []
+details.append(input("Lowercase characters: ").lower())
+details.append(input("Uppercase characters: ").lower())
+details.append(input("Numbers: ").lower())
+details.append(input("Symbols: ").lower())
+
+print(generating_pass(number, details))
